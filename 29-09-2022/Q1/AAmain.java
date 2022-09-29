977160af566ff55e445841ecb4de8cd9ce229fdf
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-09-29 13:51:11 +0530

    Create AAmain.java

---
+++ 29-09-2022/Q1/AAmain.java
@@ -0,0 +1,14 @@
+/*
+1… Write the following code:
+1. A class named Arithmetic with a method named add that takes integers as
+parameters and returns an integer denoting their sum.
+A class named Adder that inherits from a superclass named Arithmetic
+*/
+package com.pack;
+
+public class AAmain {
+	public static void main(String args[]) {
+Adder a=new Adder();
+System.out.println("Sum is "+a.add(20,10));
+}
+}
