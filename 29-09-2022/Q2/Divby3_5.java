00b1ed70b0ad55fecbe6e7b8792e2e5004210e39
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-09-29 14:06:41 +0530

    Update Divby3_5.java

--- 29-09-2022/Q2/Divby3_5.java
+++ 29-09-2022/Q2/Divby3_5.java
@@ -3,10 +3,24 @@ package com.pack;
 
 public class Divby3_5 {
 public static void main(String args[]) {
-	System.out.println("The numbers between 1 to 100 which are divisible by 3, 5 and by both:");
+	System.out.println("The numbers between 1 to 100 which are divisible by 3:");
 	for (int i=1;i<=100;i++) {
+		if(i%3==0)
+			System.out.println(i);
+
+		
+	}System.out.println("The numbers between 1 to 100 which are divisible by 5 :");
+	for (int i=1;i<=100;i++) {
+		if(i%5==0)
+			System.out.println(i);
+		
+		
+	}System.out.println("The numbers between 1 to 100 which are divisible by 3 and 5 :");
+	for (int i=1;i<=100;i++) {
+		
 		if(i%3==0 && i%5==0)
 			System.out.println(i);
+		
 	}
 }
 }
