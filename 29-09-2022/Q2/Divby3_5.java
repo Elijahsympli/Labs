1c633eab44089838bf087a78a16ce2fdc04de76b
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-09-29 13:50:20 +0530

    Create Divby3_5.java

---
+++ 29-09-2022/Q2/Divby3_5.java
@@ -0,0 +1,12 @@
+//Write a Java program to print numbers between 1 to 100 which are divisible by 3, 5 and by both.
+package com.pack;
+
+public class Divby3_5 {
+public static void main(String args[]) {
+	System.out.println("The numbers between 1 to 100 which are divisible by 3, 5 and by both:");
+	for (int i=1;i<=100;i++) {
+		if(i%3==0 && i%5==0)
+			System.out.println(i);
+	}
+}
+}
