33b25de0a715d5131ae151f8abcc27790e29871d
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-10-14 11:01:21 +0530

    Update Employee.java

--- 13-10-2022/Q1/Employee.java
+++ 13-10-2022/Q1/Employee.java
@@ -3,23 +3,18 @@ public class Employee {
 long employeeId,employeePhone;
 String employeeName,employeeAddress;
 double basicSalary;
-double specialAllowance;
-double Hra;
-Employee(){
-	specialAllowance=250.80;
-	Hra=1000.50;
-}
+double specialAllowance=250.80;
+double Hra=1000.50;
+
 Employee(long Id,String Name,String Address,long phone)
-{specialAllowance=250.80;
-Hra=1000.50;
+{
 	employeeId=Id;
 	employeeName=Name;
 	employeeAddress=Address;
 	employeePhone=phone;
 }
 Employee(long Id,String Name,String Address,long phone,double salary)
-{specialAllowance=250.80;
-Hra=1000.50;
+{
 	employeeId=Id;
 	employeeName=Name;
 	employeeAddress=Address;
