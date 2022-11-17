347631be9c7d6fbfef3cb0837e229a821a206122
Elijahsympli <111556240+Elijahsympli@users.noreply.github.com>
2022-11-17 14:33:48 +0530

    Update CalculatorSimulator.java

--- 17-11-2022/Q1/CalculatorSimulator.java
+++ 17-11-2022/Q1/CalculatorSimulator.java
@@ -4,7 +4,7 @@ public class CalculatorSimulator {
 
 	public static void main(String[] args) throws CountryNotValidException, EmployeeNameInvalidException, TaxNotEligibleException {
 TaxCalculator tc=new TaxCalculator();
-System.out.println("Tax amount is:"+(tc.calculateTax("pil", true, 30000)));
+System.out.print("Tax amount is "+(tc.calculateTax("pil", true, 30000)));
 	
 	}
 
